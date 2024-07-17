3d05186d0f2aa83302ddbcd8f8112802bf08ca4a
Bruno Correa <brunosantos6ft@gmail.com>
2024-07-17 10:42:15 -0300

    chore: Add fallback

--- src/index.ts
+++ src/index.ts
@@ -43,7 +43,7 @@ const calculateShipping = async (
 
 app.post("/proxy/shipment/calculate", calculateShipping);
 
-app.listen({ port: process.env.PORT }, (err, address) => {
+app.listen({ port: process.env.PORT || 3000 }, (err, address) => {
   if (err) console.error(err);
   console.log(`app listening at ${address}`);
 });
